22df67e124fc2f956ea434438e73313d2982622a
Eurico Sa Fernandes <eurico@ponto.ws>
2016-02-07 13:18:37 +0100

    thumb

--- templates/content-thumb.php
+++ templates/content-thumb.php
@@ -1,9 +1,5 @@
-<?php
-$thumbID = get_the_post_thumbnail( $post->ID, 'thumb' );
-?>
-
 <?php if ( has_post_thumbnail() ) { ?>
-	<div id="thumb_<?php echo basename(get_permalink()); ?>" class="preview"><?php echo "$thumbID"; ?></div>
+	<div id="thumb_<?php echo basename(get_permalink()); ?>" class="preview"><?php the_post_thumbnail('thumb'); ?></div>
 <?php } else {?>
 	<!--<div class="preview"><img src="<?php bloginfo('template_url'); ?>/assets/img/preview.png" /></div>-->
 <?php } ?>
