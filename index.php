e57b77fe6e536849a12ffc9b9edbb6b304e8d4a2
Eurico Sa Fernandes <eurico@ponto.ws>
2016-03-10 13:12:52 +0100

    no message

--- index.php
+++ index.php
@@ -3,47 +3,11 @@
 <!--Projects-->
 <section id="projects">
 
-<!-- <div class="section_inside">
-	<a class="p-link" href="<?php echo home_url(); ?>/projects/"></a>
-	<div class="page-title">
-		<h1>Projects</h1>
-	</div>
-	<div class="p-wrap">
-		<div class="p-single">
-			<?php get_template_part('templates/page','projects'); ?>
-		</div>
-	</div>
-	<div class="single_post">
-		<div class="page-title">
-			<h1 class="single-close">Close</h1>
-		</div>
 
-		<div class="single_load"><div class="loader"></div></div>
-		<div class="p-wrap">
-			<div class="p-single">
-				
-			</div>
-		</div>
-	</div>
-</div>
-<?php get_template_part('templates/page','projects-thumbs'); ?>
- -->
 </section>
 
 <!--Programme-->
 <section id="programme">
-<!-- 
-<div class="section_inside">
-	<a class="p-link" href="<?php echo home_url(); ?>/programme/"></a>
-	<div class="page-title">
-		<h1>Programme</h1>
-	</div>
-	<div class="p-wrap">
-		<div class="p-single">
-			<?php get_template_part('templates/page','programme'); ?>
-		</div>
-	</div>
-</div> -->
 
 </section>
 
