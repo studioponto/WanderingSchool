5f7c4bf977f18e9cb87e0a9870de5e42a2186f53
Eurico Sa Fernandes <eurico@ponto.ws>
2016-03-22 16:35:32 +0100

    no message

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <a class="dad_link" href="http://dirtyartdepartment.com" target="_blank">
 <img src="<?php bloginfo('template_url'); ?>/assets/img/background/sticker_3.png"></a>
 
-<div class="tws_shadow tws_shadow1"></div>
-<div class="tws_shadow tws_shadow2"></div>
+<!-- <div class="tws_shadow tws_shadow1"></div>
+<div class="tws_shadow tws_shadow2"></div> -->
